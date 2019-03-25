27ef3bfe279890c49b285ceb0eecc146e2f368d1
justin <jlouie10@users.noreply.github.com>
2019-03-24 23:49:47 -0500

    Replaced game variables with game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
 // Declare variables
-var words = ["spaghetti", "pizza", "steak", "salad", "fried chicken", "burrito"];
-var wins = 0;
-var currentWord = "new game";
-var guesses = 0;
+var game = {
+    wins: 0,
+    theme: "food",
+    wordsPack: ["spaghetti", "pizza", "steak", "salad", "fried chicken", "burrito"],
+    word: "new game",
+    guesses: 0
+}
     
 // Grabs a reference to the <span>
-var winsText = document.getElementById("wins");
-var currentWordText = document.getElementById("current-word");
-var guessesText = document.getElementById("guesses");
-var lettersText = document.getElementById("letters");
+var text = {
+    wins: document.getElementById("wins"),
+    word: document.getElementById("word"),
+    guesses: document.getElementById("guesses"),
+    letters: document.getElementById("letters")
+}
 
 // This function is run whenever the user presses a key
 document.onkeyup = function(event) {
@@ -16,25 +21,25 @@ document.onkeyup = function(event) {
     // Determines which key was pressed.
     var userGuess = event.key;
       
-    if (guesses === 0) {
+    if (game.guesses === 0) {
         // Start a new game
         newGame();
     }
     else {
         // Continue current game
-        guesses--;
+        game.guesses--;
     }
       
-    // console.log("The current word is " + currentWord);
-    // console.log("You have " + wins + " wins");
-    // console.log("There are " + guesses + " left");
-    // console.log("----------");
+    console.log("The current word is " + game.word);
+    console.log("You have " + game.wins + " wins");
+    console.log("There are " + game.guesses + " left");
+    console.log("----------");
 };
       
 function newGame() {
     // Randomly chooses a choice from the words array
-    currentWord = words[Math.floor(Math.random() * words.length)];
+    game.word = game.wordsPack[Math.floor(Math.random() * game.wordsPack.length)];
 
     // Sets the number of guesses to the length of the current word
-    guesses = currentWord.length - 1;
+    game.guesses = game.word.length - 1;
 }
