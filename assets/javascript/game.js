2796a1daf7e9620a93e4eac7ddda973948e9d0e6
justin <jlouie10@users.noreply.github.com>
2019-03-30 23:01:09 -0500

    Hide certain text when not in use

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,39 @@
 // Declare variables
+var difficulty = 2; // Set to 1 or 2 (harder)
+
 var wins = 0;
+var tillWin = 1; // Initialize to 1 so win criteria is not met when page is loaded
+var firstGame = 1;
+var isCorrect = "";
 var word = {
     current: "",
     previous: "",
     pack: ["risotto", "prawns", "scallops", "wellington", "filet", "mushrooms", "potato", "pudding", "burger", "salmon", "lobster", "short rib", "omlette", "meatballs", "fried chicken", "scrambled eggs", "bean dip", "porkchop", "sticky ribs", "sliders", "pasta", "roast", "roast beef", "curry", "chorizo", "salad", "carrot cake", "roast duck", "gnocchi", "arancini", "shawarma", "sea bass", "caviar", "pancakes", "gazpacho", "roasted squash", "jerk chicken", "apple crumble", "chips", "bisque", "caesar salad", "carpaccio", "pork belly", "monkfish", "beef tartare", "venison", "sweetbread", "pigeon", "veal", "lamb chop"],
-    masked: [], // !!! Combine array and string !!!
-    string: ""
+    packEasy: ["risotto", "scallops", "steak", "burger", "eggs"],
+    masked: [],
+    string: "",
 };
 var guess = {
     left: 0,
     letters: [],
-    string: ""
+    string: "",
 };
 
 // Grabs a reference to the <span>
 var text = {
     wins: document.getElementById("wins"),
     wordCurrent: document.getElementById("word-current"),
-    wordPrevious: document.getElementById("word-previous"),
+    wordPrevious: document.getElementById("word-previous-first"),
     guesses: document.getElementById("guesses"),
-    letters: document.getElementById("letters")
+    letters: document.getElementById("letters"),
+    instructions: document.getElementById("instructions-text"),
+    wordPreviousSecond: document.getElementById("word-previous-second")
 };
 
-console.log(word.pack.length);
 // Start a new game when page is loaded
 newGame();
+
+text.instructions.textContent = "Press any key to get started";
 
 // This function is run whenever the user presses a key
 document.onkeyup = function (event) {
@@ -35,7 +44,14 @@ document.onkeyup = function (event) {
     // Validate keypress
     if (validateInput(userGuess) === true) {
         determineMatch(userGuess);
+
+        // If first keypress in first game, clear the instructions
+        if (firstGame === 1) {
+            text.instructions.textContent = " ";
+            firstGame = 0;
+        }
         newGame();
+
     }
 }
 
@@ -49,7 +65,14 @@ function newGame() {
         word.previous = word.current;
 
         // Randomly chooses a choice from the word pack
-        word.current = word.pack[Math.floor(Math.random() * word.pack.length)];
+        if (difficulty === 1) {
+            word.current = word.packEasy[Math.floor(Math.random() * word.packEasy.length)];
+        }
+        else {
+            word.current = word.pack[Math.floor(Math.random() * word.pack.length)];
+        }
+        
+        console.log("The current word is: " + word.current);
 
         // Sets the number of guesses
         guess.left = word.current.length;
@@ -71,21 +94,32 @@ function newGame() {
             }
         }
 
+        tillWin = guess.left;
+
         // Resets the array containing guessed letters
-        guess.letters = [];
+        guess.letters = [" "];
 
         // Refresh the display at the start of a new game
-        initializeDisplay();
+        initDisplay();
     }
     // else continue current game
 }
 
-function initializeDisplay() {
+function initDisplay() {
     text.wins.textContent = wins;
-    text.wordPrevious.textContent = word.previous;
     text.guesses.textContent = guess.left;
     updateLetters(word.masked, word.string, text.wordCurrent);
     updateLetters(guess.letters, guess.string, text.letters);
+
+    // If first game, do not display previous word
+    if (firstGame === 1) {
+        text.wordPrevious.textContent = "";
+        text.wordPreviousSecond.textContent = "";
+    }
+    else {
+        text.wordPrevious.textContent = isCorrect;
+        text.wordPreviousSecond.textContent = word.previous;
+    }
 }
 
 function validateInput(input) {
@@ -118,16 +152,21 @@ function determineMatch(letter) {
                 // User guessed correctly
                 word.masked[i] = letter;
                 correctGuess = true;
+                tillWin--;
             }
         }
     }
 
-    // If user guesses no letters, count down
+    // If user guesses incorrectly, count down
     if (alreadyGuessed === false) {
         if (correctGuess === false) {
             guess.left--;
             guess.letters[guess.letters.length] = letter;
             text.guesses.textContent = guess.left;
+
+            if (guess.left === 0) {
+                isCorrect = "Come on! It's supposed to be ";
+            }
 
             // Update display only when an incorrect guess is made
             updateLetters(guess.letters, guess.string, text.letters);
@@ -142,19 +181,10 @@ function determineMatch(letter) {
 function determineWin() { // !!! Revise this into a count down from word length !!!
     var winStatus = false;
 
-    for (i = 0; i < word.masked.length; i++) {
-
-        // Check for character mask
-        if (word.masked[i] === "_") {
-            i = word.masked.length;
-            winStatus = false;
-        }
-
-        // Once the array has been iterated through and no characters are masked, it's a win
-        else if (i === (word.masked.length - 1)) {
-            winStatus = true;
-            wins++;
-        }
+    if (tillWin === 0) {
+        winStatus = true;
+        wins++;
+        isCorrect = "That's a perfect ";
     }
 
     return winStatus;
